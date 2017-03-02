9a2b7eb27c255f6c2320b845d9abbef0675a2297
HelenaYounes <younesq.helena@gmail.com>
2017-03-02 12:41:09 -0700

    added borders

--- tictactoe.js
+++ tictactoe.js
@@ -8,7 +8,7 @@ function test(e) {
 //  debugger;
   // var n = e.target.dataset.num;
   var n = e.target.getAttribute('data-num');
-  if(n=='01'){
+  if( n.innerHTML!='X' || n.innerHTML!='O'){
     e.target.innerHTML = 'X';
   }
   alert(n);
