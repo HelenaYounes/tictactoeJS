d73bc8a4993158060ef398e37cc0613973c78a18
HelenaYounes <younesq.helena@gmail.com>
2017-03-03 14:34:22 -0700

    add method to alert when winning move

--- tictactoe.js
+++ tictactoe.js
@@ -1,38 +1,45 @@
 var count= 0;
-
 var emptySymbol = '&nbsp;';
+
+
 var onLoad = function() {
   var board = document.querySelector('.tic');
-  board.addEventListener('click', test);
-  var resetall = document.querySelector('.buttons');
+  board.addEventListener('click', test, checkNearbyCell);
+  var resetall = document.querySelector('.reseter');
   resetall.addEventListener('click',reset);
+  // var check = document.querySelector('.checker');
+  // check.addEventListener('click',checkNearbyCell);
 }
 
 function test(e) {
- debugger;
+// debugger;
 // var n = e.target.dataset.num;
+// var element = e.target.getAttribute('data-num');
+ var i = 0;
+ var j= 0;
  count++;
   n = e.target.getAttribute('data-num');
-  var cell = e.target;
-  var content = cell.innerHTML;
-  var element = e.target.getAttribute('data-num');
+  var cellClicked = e.target;
+  var content = cellClicked.innerHTML;
   if(count%2==0){
-    if(cell.innerHTML ==emptySymbol){
-      cell.innerHTML = 'X';
+    if(cellClicked.innerHTML ==emptySymbol){
+      cellClicked.innerHTML = 'X';
     }
   }
   else{
-    if(cell.innerHTML ==emptySymbol){
-      cell.innerHTML  = 'O';
+    if(cellClicked.innerHTML ==emptySymbol){
+      cellClicked.innerHTML  = 'O';
     }
   }
+  checkNearbyCell();
 }
 
+
 function reset(){
-  debugger;
-  var table = document.getElementById('myTable');
-  var i =0;
+  //debugger;
+  var i = 0;
   var j = 0;
+  var table = document.getElementById('myTable');
   for ( i = 0 ; i< table.rows.length ; i++) {
     var cell = table.rows[i].cells;
     for(j = 0; j<cell.length; j++){
@@ -40,5 +47,30 @@ function reset(){
     }
   }
  }
- 
+
+function checkNearbyCell(){
+  //debugger;
+  var table = document. getElementById('myTable');
+  for(i = 1; i<table.rows.length - 1; i++){
+    var cellUp = table.rows[i-1];
+    var cellDowm = table.rows[i+1];
+    var cell = table.rows[i].cells;
+    for(j = 1; j<cell.length ; j++){
+      if(cell[j].innerHTML != emptySymbol && cell[j].innerHTML == cell[j+1].innerHTML){
+        if (cell[j].innerHTML == cell[j-1].innerHTML){
+          alert("you win");
+        }
+      }
+      else if (cell[j].innerHTML != emptySymbol && cell[j].innerHTML == cellUp[j].innerHTML ){
+        if(cell[j].innerHTML == cellDowm[j].innerHTML){
+          alert('You win');
+        }
+      }
+      else{
+
+      }
+     }
+   }
+ }
+
 window.addEventListener('load', onLoad);
