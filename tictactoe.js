1e60785845da5ac9496843ba4a97c7dcdf481964
HelenaYounes <younesq.helena@gmail.com>
2017-03-06 21:20:56 -0700

    added wining combination and check for win

--- tictactoe.js
+++ tictactoe.js
@@ -1,37 +1,45 @@
-var count= 0;
+var clicks= 0;
 var emptySymbol = '&nbsp;';
+var count = 0;
+var player = 1;
+var boardLength = 4;
+var boardHeight = 4;
 
 var onLoad = function() {
   var board = document.querySelector('.tic');
-  board.addEventListener('click', test, checkNearbyCell);
+  board.addEventListener('click', play);
   var resetall = document.querySelector('.reseter');
   resetall.addEventListener('click',reset);
-  // var check = document.querySelector('.checker');
-  // check.addEventListener('click',checkNearbyCell);
 }
 
-function test(e) {
-// var n = e.target.dataset.num;
-// var element = e.target.getAttribute('data-num');
+function play(e) {
   var i = 0;
   var j= 0;
-  count++;
+  clicks++;
   n = e.target.getAttribute('data-num');
   var cellClicked = e.target;
   var content = cellClicked.innerHTML;
-  if(count%2==0){
-    if(cellClicked.innerHTML ==emptySymbol){
+  if (clicks % 2) {
+    player = 1;
+  } else {
+    player = 2;
+  }
+  if(player == 1){
+    if(content ==emptySymbol){
       cellClicked.innerHTML = 'X';
     }
-  }
-  else{
-    if(cellClicked.innerHTML ==emptySymbol){
+  } else if (player == 2){
+    if(content ==emptySymbol){
       cellClicked.innerHTML  = 'O';
     }
   }
-  checkNearbyCell();
+  // writeToBoard(something);
+  var board = readBoard();
+  var win = checkBoard(board);
+  if (win) {
+    alert('player ' + player + ' won');
+  }
 }
-
 
 function reset(){
   var i = 0;
@@ -45,25 +53,92 @@ function reset(){
   }
  }
 
-function checkNearbyCell(){
-  var table = document. getElementById('myTable');
-  for(i = 1; i<table.rows.length - 1; i++){
-    var cellUp = table.rows[i-1];
-    var cellDowm = table.rows[i+1];
-    var cell = table.rows[i].cells;
-    for(j = 1; j<cell.length ; j++){
-      if(cell[j].innerHTML != emptySymbol && cell[j].innerHTML == cell[j+1].innerHTML){
-        if (cell[j].innerHTML == cell[j-1].innerHTML){
-          alert("you win");
-        }
-      }
-      else if (cell[j].innerHTML != emptySymbol && cell[j].innerHTML == cellUp[j].innerHTML ){
-        if(cell[j].innerHTML == cellDowm[j].innerHTML){
-          alert('You win');
-        }
-      }
+ function checkBoard(board) {
+    return checkRows(board) || checkCols(board) || checkDiag(board);
+ }
+
+function getCell(i, j) {
+  var selector = '.row[data-row="'+i+'"] .cells[data-col="'+j+'"]';
+  var cell = document.querySelector( selector );
+  return cell;
+}
+
+function getCol(board, c) {
+  var col = [];
+  for (var i = 0; i < boardHeight; i++) {
+    col.push(board[i][c]);
+  }
+  return col;
+}
+
+function checkCols(board) {
+  for (var i = 0; i < boardLength; i++) {
+    var col = getCol(board, i);
+    if ( checkCells(col) ) {
+      return true;
     }
   }
+  return false;
+}
+
+function checkRows(board) {
+  for( var i = 0; i<board.length; i++){
+    if (checkCells(board[i])) {
+      return true;
+    };
+  }
+  return false;
+}
+
+function checkCells(cells) {
+  if (cells[0] === emptySymbol) {
+    return false;
+  }
+  for(var i = 0; i<cells.length-1; i++){
+    var cell = cells[i];
+    if( cell != cells[i+1]){
+      return false;
+    }
+  }
+  return true;
+}
+
+function getDiag1(board) {
+  var diag = [];
+  for (var i = 0; i < boardLength; i++) {
+    diag.push(board[i][i]);
+  }
+  return diag;
+}
+
+function getDiag2(board) {
+  var diag = [];
+  for (var i = 0; i < boardLength; i++) {
+    diag.push(board[i][boardLength-i-1]);
+  }
+  return diag;
+}
+
+function checkDiag(board) {
+  var diag1 = getDiag1(board);
+  var diag2 = getDiag2(board);
+  return checkCells(diag1) || checkCells(diag2);
+}
+
+function readRow(r) {
+  var row = [];
+  for (var i = 0; i < boardHeight; i++) {
+    row.push( getCell(r, i).innerHTML );
+  }
+  return row;
+}
+
+function readBoard() {
+  var board = [];
+  for (var i = 0; i < boardLength; i++) {
+    board[i] = readRow(i);
+  }
+  return board;
 }
 
 window.addEventListener('load', onLoad);
